1e7bb0f6f52fe04ad349a9fdf3135eaf3cfb269e
Dave Calnan <d@ve.ie>
2018-10-20 21:12:55 +0100

    Fix Error: You are calling concat on a terminating link, which will have no effect

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,9 +1,11 @@
-import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const GRAPHCMS_API = 'https://api-euwest.graphcms.com/v1/cjmi83q8227ho01b9abzzxgrw/master'
+const token =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJ0b2tlbklkIjoiZWI2MWI2ZWUtOWVjOS00Mzk5LTkwMzItYjE3ZmFjMjg2MzRlIn0.sCN5lhrn2sBO1nnhJgpRyvde9B07IamkErjpPtyq3d4'
 
 export default () => ({
-  link: new HttpLink({ uri: GRAPHCMS_API }),
+  httpEndpoint: GRAPHCMS_API,
+  getAuth: () => `Bearer ${token}`,
   cache: new InMemoryCache()
-})
+});
